c6502feac6b5687b108d64addbe0c6c3303cc34f
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2022-12-04 06:58:39 +0000

    added remaining questions into functions that fire on the answer selected by the user

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,21 +1,63 @@
 document.getElementById("start").addEventListener("click", startQuiz);
+var unHide = document.getElementById("questions");
 
 var time = 60;
 
 var score = 0;
-console.log(score);
+
 function correctAnswer() {
   score += 10;
   console.log(score);
+  document.getElementById("questions").innerHTML = `
+  <div class="wrapper">
+  <h2>How do you get 22?</h2>
+  <button onclick="incorrectAnswerTwo()">1. 2++</button>
+  <button onclick="questionTwo()">2. 2 + 2</button>
+  <button onclick="incorrectAnswerTwo()">3. 2 += 2</button>
+
+  </div>`;
+}
+
+function questionTwo() {
+  score += 10;
+  console.log(score);
+  document.getElementById("questions").innerHTML = `
+  <div class="wrapper">
+  <h2>What does 'ES6' stand for?</h2>
+  <button onclick="">1. Elegant Syntax 6</button>
+  <button onclick="">2. ECMA Script 6</button>
+  <button onclick="">3. Elder Scrolls 6</button>
+
+  </div>`;
 }
 
 function incorrectAnswer() {
-  console.log(score);
   score -= 10;
   console.log(score);
+  document.getElementById("questions").innerHTML = `
+  <div class="wrapper">
+  <h2>How do you get 22?</h2>
+  <button onclick="incorrectAnswerTwo()">1. 2++</button>
+  <button onclick="questionTwo()">2. 2 + 2</button>
+  <button onclick="incorrectAnswerTwo()">3. 2 += 2</button>
+  </div>`;
+}
+
+function incorrectAnswerTwo() {
+  score -= 10;
+  console.log(score);
+  document.getElementById("questions").innerHTML = `
+  <div class="wrapper">
+  <h2>What does 'ES6' stand for?</h2>
+  <button onclick="">1. Elegant Syntax 6</button>
+  <button onclick="">2. ECMA Script 6</button>
+  <button onclick="">3. Elder Scrolls 6</button>
+
+  </div>`;
 }
 
 function startQuiz() {
+  document.getElementById("start-screen").classList.add("hide");
   document.getElementById("questions").innerHTML = `
   <div class="wrapper">
   <h2>Which of these is NOT a valid JavaScript data type?</h2>
@@ -25,6 +67,5 @@ function startQuiz() {
   <button onclick="incorrectAnswer()">4. Null</button>
   </div>
   `;
-  var unHide = document.getElementById("questions");
   unHide.classList.remove("hide");
 }
